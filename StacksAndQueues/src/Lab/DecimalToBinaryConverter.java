95ed74e4ef2c5c418bc6016e8a1a079e7b66cfc1
tsvetantodorov <tstodorofff@gmail.com>
2023-02-07 18:10:30 +0200

    Adding DecimalToBinaryConverter

---
+++ StacksAndQueues/src/Lab/DecimalToBinaryConverter.java
@@ -0,0 +1,30 @@
+package Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class DecimalToBinaryConverter {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<Integer> bits = new ArrayDeque<>();
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        if (num == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        while (num > 0) {
+            int bit = num % 2;
+            bits.push(bit);
+            num = num / 2;
+        }
+
+        while (!bits.isEmpty()) {
+            System.out.print(bits.pop());
+        }
+    }
+}
