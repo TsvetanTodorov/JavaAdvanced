6c5704a4e2b8baa6f7ffce0cd00f4f358d72b697
tsvetantodorov <tstodorofff@gmail.com>
2023-03-10 12:32:15 +0200

    Adding UniqueUsernames

---
+++ JavaAdvanced/src/SetsAndMaps/Exercise/UniqueUsernames.java
@@ -0,0 +1,26 @@
+package SetsAndMaps.Exercise;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class UniqueUsernames {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = Integer.parseInt(scanner.nextLine());
+        Set<String> usernames = new LinkedHashSet<>();
+
+        for (int i = 0; i < counter; i++) {
+
+            String currentUsername = scanner.nextLine();
+
+            usernames.add(currentUsername);
+
+        }
+        for (String user : usernames) {
+            System.out.println(user);
+        }
+    }
+}
