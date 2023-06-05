a681e9af69cbf511601ce752132b6b07546d8262
tsvetantodorov <tstodorofff@gmail.com>
2023-06-05 15:22:37 +0300

    Adding CardSuits

---
+++ JavaAdvanced/src/OOP/WorkingWithAbstraction/Exercise/CardSuit/CardSuits.java
@@ -0,0 +1,9 @@
+package OOP.WorkingWithAbstraction.Exercise.CardSuit;
+
+public enum CardSuits {
+
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES
+}
---
+++ JavaAdvanced/src/OOP/WorkingWithAbstraction/Exercise/CardSuit/Main.java
@@ -0,0 +1,13 @@
+package OOP.WorkingWithAbstraction.Exercise.CardSuit;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CardSuits[] values = CardSuits.values();
+        System.out.println("Card Suits:");
+        for (CardSuits value : values) {
+            System.out.printf("Ordinal value: %d; Name value: %s\n", value.ordinal(), value.name());
+        }
+
+    }
+}
