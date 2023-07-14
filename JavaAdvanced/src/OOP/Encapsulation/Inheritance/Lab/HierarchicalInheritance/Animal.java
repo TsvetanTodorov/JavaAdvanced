7f0543fe9e2d404a1db1f271fcab6322d9d88772
tsvetantodorov <tstodorofff@gmail.com>
2023-07-14 11:28:19 +0300

    Adding HierarchicalInheritance

---
+++ JavaAdvanced/src/OOP/Encapsulation/Inheritance/Lab/HierarchicalInheritance/Animal.java
@@ -0,0 +1,7 @@
+package OOP.Encapsulation.Inheritance.Lab.HierarchicalInheritance;
+
+public class Animal {
+    public void eat(){
+        System.out.println("eating...");
+    }
+}
---
+++ JavaAdvanced/src/OOP/Encapsulation/Inheritance/Lab/HierarchicalInheritance/Dog.java
@@ -0,0 +1,8 @@
+package OOP.Encapsulation.Inheritance.Lab.HierarchicalInheritance;
+
+public class Dog extends Animal{
+
+    public void bark(){
+        System.out.println("barking...");
+    }
+}
