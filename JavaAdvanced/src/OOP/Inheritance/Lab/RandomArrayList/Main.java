ea4a5ae63f8ea54414ba17caffbfe84dc4e7b0a1
tsvetantodorov <tstodorofff@gmail.com>
2023-07-14 13:18:35 +0300

    Adding RandomArrayList

---
+++ JavaAdvanced/src/OOP/Inheritance/Lab/RandomArrayList/Main.java
@@ -0,0 +1,16 @@
+package OOP.Inheritance.Lab.RandomArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        RandomArrayList<Integer>randomArrayList = new RandomArrayList<>();
+
+        randomArrayList.add(10);
+        randomArrayList.add(20);
+        randomArrayList.add(30);
+
+        System.out.println(randomArrayList.getRandomElement());
+        System.out.println(randomArrayList.getRandomElement());
+    }
+}
