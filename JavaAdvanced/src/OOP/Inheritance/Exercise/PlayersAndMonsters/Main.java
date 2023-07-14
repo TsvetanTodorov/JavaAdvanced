3b5f3c9f4de1653f12ea71b61f327ac3df0effd9
tsvetantodorov <tstodorofff@gmail.com>
2023-07-14 15:29:29 +0300

    Adding PlayersAndMonsters

---
+++ JavaAdvanced/src/OOP/Inheritance/Exercise/PlayersAndMonsters/Main.java
@@ -0,0 +1,11 @@
+package OOP.Inheritance.Exercise.PlayersAndMonsters;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Wizard wizard = new Wizard("Tsetso", 20);
+        SoulMaster soulMaster = new SoulMaster("Tsetso", 20);
+
+        System.out.println(soulMaster);
+    }
+}
